f08dfc55ad172b026f7b6864eb06d5cb9e1a6365
Xuerui-Yang <54955921+Xuerui-Yang@users.noreply.github.com>
2019-09-05 19:00:29 +0100

    Add files via upload

--- example.py
+++ example.py
@@ -1,24 +1,6 @@
 import pandas as pd
-import numpy as np
+
 from jmcm import JointModel
-"""
-### data preprocessing
-def ctr_std(arr):
-    sd=np.std(arr)
-    mn=np.mean(arr)
-    return (arr-mn)/sd
-
-time=ctr_std(df.loc[:,"edate"])
-gender=df.loc[:,"gender"]-1
-age=df.loc[:,"eventdate"].apply(f)-df.loc[:,"yearofbirth"]
-bmi=df.loc[:,"bmi"]
-y=1/df.loc[:,"hba1c"]
-
-data=pd.concat([patid,gender,time,age,bmi,y],axis=1)
-data.columns=['patid','gender','time','age','bmi','y']
-
-data.to_csv('data_new.txt', header=True, index=False, sep='\t', mode='a')
-"""
 
 ### import the data
 diabetes = pd.read_table("data_new.txt")
