db5e4ab3880763c894d08eb366def2f7b26816d5
Xuerui-Yang <54955921+Xuerui-Yang@users.noreply.github.com>
2019-10-02 09:30:14 +0800

    Update example.py

--- example/example.py
+++ example/example.py
@@ -6,7 +6,7 @@ from jmcm import JointModel
 diabetes = pd.read_table("data_new.txt")
 
 ### fit the model
-JM=JointModel(diabetes, 'y|patid|time~1|1', (3,3,3),optim_meth='default')
+JM=JointModel(diabetes, 'y|patid|time~1|1', (3,3,3), optim_meth='default')
 
 ### summarise the estimation
 JM.summary()
