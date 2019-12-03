6616f5efea0cd1f96ad2aefe9de43aeae2587b60
Xuerui-Yang <54955921+Xuerui-Yang@users.noreply.github.com>
2019-12-03 08:43:21 +0800

    Update test1_generator.py

--- tests/test1_generator.py
+++ tests/test1_generator.py
@@ -1,11 +1,13 @@
 import pytest
 import pandas as pd
 
-from data_generator import generator
+from data_generator import simulate,sim_par
 
 def test():
     m=3
-    df=generator(m)
+    bta,lmd,gma=sim_par(3,3,3)
+    assert len(bta)==3
+    df=simulate(bta,lmd,gma,m)
     assert isinstance(df, pd.DataFrame)
     assert df.shape[1]==3
     assert df.iloc[-1]['id']==m-1
