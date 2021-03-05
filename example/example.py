ab36d7279f1363fccaf8c689162c713aab1adf0e
Xuerui-Yang <54955921+Xuerui-Yang@users.noreply.github.com>
2021-03-05 09:07:13 +0800

    Update example.py

--- example/example.py
+++ example/example.py
@@ -3,10 +3,11 @@ import pandas as pd
 from jmcm import JointModel
 
 ### import the data
-diabetes = pd.read_table("data_new.txt")
+df=pd.read_csv("aids.csv")
+df['cd4s']=np.sqrt(df['cd4'])
 
 ### fit the model
-JM=JointModel(diabetes, 'y|patid|time~1|1', (3,3,3), optim_meth='default')
+JM=JointModel(df, 'cd4s|id|time~1|1',(8,3,3), optim_meth='default')
 
 ### summarise the estimation
 JM.summary()
